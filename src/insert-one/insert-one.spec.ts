5b6441d451facc9a594f755eeb3b286869dfbe0b
joav <jim.acero.valcarcel@gmail.com>
2023-02-15 14:05:07 -0500

    Added insertOne logic

--- src/insert-one/insert-one.spec.ts
+++ src/insert-one/insert-one.spec.ts
@@ -1,3 +1,39 @@
+import { insertOne } from "./insert-one";
+
+const insertedId = "FAKE";
+jest.mock(
+  "../fetch/create-query",
+  () => ({
+    createQuery: () => ({}),
+  }),
+  { virtual: true },
+);
+jest.mock(
+  "../fetch/create-options",
+  () => ({
+    createOptions: () => ({}),
+  }),
+  { virtual: true },
+);
+jest.mock(
+  "../fetch/uri",
+  () => ({
+    uri: () => "",
+  }),
+  { virtual: true },
+);
+jest.mock(
+  "../fetch/fetch",
+  () => ({
+    doFetch: () => Promise.resolve({insertedId}),
+  }),
+  { virtual: true },
+);
+
 describe("InsertOne", () => {
-  it("should...", () => {});
+  it("should insertOne", async () => {
+    const result = await insertOne("FAKE", {});
+
+    expect(result).toEqual(insertedId);
+  });
 });
--- src/insert-one/insert-one.ts
+++ src/insert-one/insert-one.ts
@@ -1,4 +1,13 @@
-export function insertOne(collection: string, document: any) {
-  console.log(collection);
-  console.log(document);
+import { createOptions } from "../fetch/create-options";
+import { createQuery } from "../fetch/create-query";
+import { doFetch } from "../fetch/fetch";
+import { uri } from "../fetch/uri";
+
+export async function insertOne(collection: string, document: object): Promise<string> {
+  console.log("Call insertOne");
+  const query = createQuery(collection, { document });
+  const options = createOptions(query);
+  const { insertedId } = await doFetch<{insertedId: string}>(uri('insertOne'), options);
+  console.log("Success call insertOne");
+  return insertedId;
 }
